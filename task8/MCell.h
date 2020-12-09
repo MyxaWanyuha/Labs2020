a2d27b49d520ccb70b50dcd53a6a80c3bb12cae0
MyxaWanyuha <sablin.2001@inbox.ru>
2020-12-10 00:03:18 +0500

    add MCell.cpp

--- task8/MCell.h
+++ task8/MCell.h
@@ -3,10 +3,10 @@ class MCell
 {
 public:
 	friend class Maze;
-	bool right() const noexcept { return m_right; }
-	bool down() const noexcept { return m_down; }
+	bool right() const;
+	bool down() const;
 private:
-	MCell() {};
+	MCell();
 	bool m_down = false;
 	bool m_right = false;
 };
