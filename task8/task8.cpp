a6c0c2530bfa0969c01a854fee52a1ca98e3a4a0
MyxaWanyuha <sablin.2001@inbox.ru>
2020-12-14 20:24:41 +0500

    task9

--- task8/task8.cpp
+++ task8/task8.cpp
@@ -1,6 +1,47 @@
 ﻿#include <iostream>
 #include "Maze.h"
+#include "MTreeNode.h"
 class Maze;
+class MTreeNode;
+
+void PrintTreeLadder()
+{
+    const int mazeDim = 5;
+    Maze maze(mazeDim, mazeDim);
+
+    for (int x = -1; x < mazeDim - 1; x++)
+    {
+        for (int i = x; i < mazeDim; i++)
+            maze.makeConnection(i, x + 1, i + 1, x + 1);
+        for (int j = x; j < mazeDim; j++)
+            maze.makeConnection(x, j, x, j + 1);
+    }
+    maze.printMaze();
+    std::cout << '\n';
+
+    auto root = MTreeNode::beginTree(0, 0);
+    for (int i = 0; i < mazeDim; i++)
+        for (int j = 0; j < mazeDim; j++)
+        {
+            if (maze.hasConnection(i, j, i + 1, j) && !root->hasChild(i + 1, j))
+                root->addChild(i + 1, j);
+            if (maze.hasConnection(i, j, i, j + 1) && !root->hasChild(i, j + 1))
+                root->addChild(i, j + 1);
+        }
+
+    for (int i = 0; i < mazeDim; i++)
+    {
+        for (int j = 0; j < mazeDim; j++)
+        {
+            auto point = root->hasChild(i, j);
+            if (point)
+                std::cout << point->distance() << ' ';
+        }
+        std::cout << '\n';
+    }
+    delete root;
+}
+
 int main()
 {
     Maze maze(5, 5);
@@ -11,5 +52,8 @@ int main()
         maze.makeConnection(iIncr, iIncr, iIncr, iIncr + 1);
     }
     maze.printMaze();
+    std::cout << '\n';
+
+    PrintTreeLadder();
     return 0;
 }
